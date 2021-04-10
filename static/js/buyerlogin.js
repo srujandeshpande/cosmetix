f24cfd95ab2c96f0b849212c066298e8d4350e33
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-04-10 18:18:16 +0530

    Added logout, fixed login

--- static/js/buyerlogin.js
+++ static/js/buyerlogin.js
@@ -15,7 +15,7 @@ $(function() {
 
 function login(data){
   $.ajax({
-    url: '/user',
+    url: '/user/',
     type: 'POST',
     data: JSON.stringify(data),
     contentType: 'application/json; charset=utf-8',
